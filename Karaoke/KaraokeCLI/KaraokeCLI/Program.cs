569178ac2268588a9192f302a16487098f62b6da
Alvaro <tsi.iamesa@gmail.com>
2017-09-18 15:03:14 -0300

    karaoke cli

---
+++ Karaoke/KaraokeCLI/KaraokeCLI/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaraokeCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = {"allthistime","endoftheworld","itsasmallworld"};
+            Console.WriteLine("Bienvenido al Karaoke");
+            while (true)
+            {
+                Console.WriteLine("Elige una Cancion");
+                Console.WriteLine("1 para Jonathan Coulton All this time");
+                Console.WriteLine("2 para End of the World");
+                Console.WriteLine("3 para Its a Small World");
+                Console.WriteLine("Ingresa 'Salir' para salir del programa");
+
+                string response = Console.ReadLine();
+
+                if (response.ToLower() == "salir") {
+                    break;
+                }
+                var song = 0;
+                if (!int.TryParse(response, out song)) {
+                    Console.WriteLine("Error, intenta de nuevo.");
+                    continue;
+                }
+                else
+                song--;
+
+                var fileName = songs[song]+".txt";
+                Console.WriteLine(fileName); 
+
+                if (!File.Exists(fileName)) {
+                    Console.WriteLine("El archivo no se encuentra, intenta de nuevo");
+                    continue;
+                }
+
+                var lines = File.ReadAllLines(fileName);
+                Console.WriteLine("==========================================");
+                foreach (var line in lines) {
+                    Singer.WriteLine(line);
+                }
+                Console.WriteLine("==========================================");
+            }
+           
+
+        }
+    }
+}
---
+++ Karaoke/KaraokeCLI/KaraokeCLI/Singer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaraokeCLI
+{
+    public class Singer
+    {
+        public static void WriteLine(string line)
+        {
+            var results = line.Split(':');
+            WriteLine(results[0],  int.Parse(results[1]));
+        }
+        public static void WriteLine(string line, int time)
+        {
+            var speed = time / line.Length;
+            foreach (var letter in line.ToCharArray()) {
+                Console.Write(letter);
+                System.Threading.Thread.Sleep(speed); 
+
+            }
+            Console.WriteLine("");
+
+
+
+            
+
+        }
+    }
+}
